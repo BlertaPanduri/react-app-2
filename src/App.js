b40232acd89edb030eddbb17d6a76d81d0780e31
BlertaPanduri <blertabajri@gmail.com>
2020-03-09 11:39:48 +0100

    firstCommit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Person from './Person/Person.js'
 import './App.css';
 
-function App() {
+
+class App extends Component {
+  state={
+    persons:[
+      {name: 'Zana', age: 22},
+      {name: 'Blerta', age: 29},
+    ],
+    showPersons: false
+  }
+  switchNameHandler = (NewName) =>{
+    this.setState({
+      persons:[
+        {name: NewName, age: 22},
+        {name: 'Blerta', age: 29},
+      ]
+      
+    })
+  }
+  nameChangedHandler = (event) => {
+    this.setState({
+      persons:[
+        {name: 'Zana', age: 22},
+        {name: event.target.value, age: 29},
+      ]
+    })
+  }
+  toggelPersonsHandler=() => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow})
+  }
+  render(){
+    const style={
+      backgraundColor:'white',
+      font:'inherit',
+      border: '1px solid blue',
+      padding:'8px',
+      cursor:'pointer'
+    };
+
+    let persons = null;
+    if(this.state.showPersons){
+      persons=(
+        <div>
+          <Person
+          name = {this.state.persons[0].name}
+          age = {this.state.persons[0].age}
+          click={this.switchNameHandler.bind(this,'Zana!')}/>
+          <Person
+          name = {this.state.persons[1].name}
+          age = {this.state.persons[1].age}
+          changed={this.nameChangedHandler}>My Hobbies: Reading</Person>
+
+        </div>
+      )
+
+    
+  }
+  
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button style={style}
+      onClick= {this.toggelPersonsHandler}>Toggle Persons</button>
+      {persons}
     </div>
   );
+}
 }
 
 export default App;
